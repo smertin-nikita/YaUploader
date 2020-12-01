b1769709557ac96dee3da9a96ee4c002fc287741
Smertin Nikita <smertin-nikita@mail.ru>
2020-12-01 18:48:02 +0500

    Added exception handling

--- main.py
+++ main.py
@@ -7,16 +7,13 @@ import os.path as op
 TOKEN_PATH = op.join(os.getcwd(), 'token')
 
 
-def get_token():
+def get_token(path):
     try:
-        with open(TOKEN_PATH, 'r') as f:
+        with open(path, 'r') as f:
             token = f.readline()
     except FileNotFoundError:
         print("File not found")
     return token
-
-
-HEADERS = {'Authorization': 'OAuth ' + get_token()}
 
 
 class YaUploader:
@@ -26,26 +23,35 @@ class YaUploader:
     def upload(self, file_path: str):
         """Метод загруджает файл file_path на яндекс диск"""
 
-        response = requests.get(
-            url='https://cloud-api.yandex.net/v1/disk/resources/upload',
-            params={'path': op.basename(file_path), 'overwrite': 'true'},
-            headers=HEADERS
-        )
-        response.raise_for_status()
+        try:
+            response = requests.get(
+                url='https://cloud-api.yandex.net/v1/disk/resources/upload',
+                params={'path': op.basename(file_path), 'overwrite': 'true'},
+                headers={'Authorization': 'OAuth ' + self.token}
+            )
+            response.raise_for_status()
+        except requests.RequestException as e:
+            print(e.response.status_code)
+            return False
 
         data = response.json()
         href = data['href']
 
-        # try:
-        with open(file_path, 'rb') as f:
-            response = requests.put(href, files={'file': f})
-            response.raise_for_status()  # TODO узнать какие ошибки возникают
-        # except:
-        #     print()
-
-        return 'Вернуть ответ об успешной загрузке'
+        try:
+            with open(file_path, 'rb') as f:
+                response = requests.put(href, files={'file': f})
+                response.raise_for_status()
+                return True
+        except FileNotFoundError:
+            print('Файл не найден')
+            return False
+        except requests.RequestException as e:
+            print(e.response.status_code)
+            return False
 
 
 if __name__ == '__main__':
-    uploader = YaUploader(TOKEN_PATH)
-    result = uploader.upload('C:\\Users\\Nikita\\Desktop\\1.txt')
+    uploader = YaUploader(get_token(TOKEN_PATH))
+    path_file = op.join('1.txt')
+    result = uploader.upload(path_file)
+    print(result)
